7064203d042115b45361edd29ee47290decf7984
Clinton Blackburn <cblackburn@edx.org>
2014-12-04 16:55:35 -0500

    Removed Unused APIs

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1563,9 +1563,7 @@ INSTALLED_APPS = (
     'survey',
 
     'lms.djangoapps.lms_xblock',
-
-    # edX Server API
-    'server_api',
+    'course_api',
 )
 
 ######################### MARKETING SITE ###############################
@@ -1943,5 +1941,5 @@ COURSE_CATALOG_VISIBILITY_PERMISSION = 'see_exists'
 COURSE_ABOUT_VISIBILITY_PERMISSION = 'see_exists'
 
 
-############# OPEN EDX SERVER API #################
-FEATURES['SERVER_API'] = False
+############# OPEN EDX COURSE API #################
+FEATURES['COURSE_API'] = False
