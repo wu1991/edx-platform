533e19da0e8b198f53e35db5777d3ce14b90c329
Muhammad Shoaib <muammad.shoaib@arbisoft.com>
2015-01-08 20:14:11 +0500

    added the structure for pdfminor

--- lms/djangoapps/shoppingcart/tests/test_pdf.py
+++ lms/djangoapps/shoppingcart/tests/test_pdf.py
@@ -41,6 +41,40 @@ class TestPdfFile(unittest.TestCase):
         """
         download the pdf file to the server
         """
+        from pdfminer.pdfparser import PDFParser
+        from pdfminer.pdfdocument import PDFDocument
+        from pdfminer.pdfpage import PDFPage
+        from pdfminer.pdfpage import PDFTextExtractionNotAllowed
+        from pdfminer.pdfinterp import PDFResourceManager
+        from pdfminer.pdfinterp import PDFPageInterpreter
+        from pdfminer.pdfdevice import PDFDevice
+        from pdfminer.layout import LAParams
+        from pdfminer.converter import PDFPageAggregator
+
+        # Open a PDF file.
+        # fp = open('mypdf.pdf', 'rb')
+        # Create a PDF parser object associated with the file object.
+        parser = PDFParser(pdf_buffer)
+        # Create a PDF document object that stores the document structure.
+        # Supply the password for initialization.
+        document = PDFDocument(parser, 'abc')
+        # Check if the document allows text extraction. If not, abort.
+        if not document.is_extractable:
+            raise PDFTextExtractionNotAllowed
+        # Create a PDF resource manager object that stores shared resources.
+        rsrcmgr = PDFResourceManager()
+        # Create a PDF device object.
+        device = PDFDevice(rsrcmgr)
+        # Create a PDF interpreter object.
+        # Set parameters for analysis.
+        laparams = LAParams()
+        # Create a PDF page aggregator object.
+        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        for page in PDFPage.create_pages(document):
+            interpreter.process_page(page)
+            # receive the LTPage object for the page.
+            device.get_result()
         with open('/edx/app/edxapp/edx-platform/%s.pdf' % filename, 'w') as f:
             myfile = File(f)
             myfile.write(pdf_buffer.getvalue())
